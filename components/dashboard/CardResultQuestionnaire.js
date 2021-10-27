6f8c865da1b0f386dd1393c69e831ad799f0d3f5
Abdallah-ben <abdallah_ben@me.com>
2021-10-27 16:33:29 +0100

    this is me

--- components/dashboard/CardResultQuestionnaire.js
+++ components/dashboard/CardResultQuestionnaire.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useRef } from 'react'
-import { Text, View, Image, Animated } from 'react-native'
+import {
+  Text,
+  View,
+  Image,
+  Animated,
+  UIManager,
+  LayoutAnimation,
+} from 'react-native'
 import PropTypes from 'prop-types'
 import theme from '../../constants/Theme'
 import FontAwesome from 'react-native-vector-icons/FontAwesome'
@@ -8,50 +15,53 @@ import { ButtonCureety } from '../index'
 import { Button } from 'react-native-elements'
 import Images from '../../constants/Images'
 import { styles } from './CardResultQuestionnaireStyle'
-
-export default function CardResultQuestionnaire (props) {
-  const { grade, bgColor, msgCHU, onOpenMsg, onCallCHU, nbrMsg, notifReport, nextReport, lastCreatedReport, text1, text2, text3, text4, text5, text6, text7, text8, text9 } = props
-
-  const firstOpacity = useRef(
-    new Animated.Value(0)
-  ).current
-
-  const secondInterpolation = useRef(
-    new Animated.Value(0)
-  ).current
-
-  const thirdInterpolation = useRef(
-    new Animated.Value(0)
-  ).current
-
-  useEffect(() => {
-    Animated.stagger(1000,
-      [
-        Animated.timing(firstOpacity,
-          {
-            toValue: 1,
-            delay: 500,
-            duration: 500,
-            useNativeDriver: true
-          }),
-        Animated.timing(secondInterpolation,
-          {
-            toValue: 20,
-            delay: 1000,
-            duration: 500,
-            useNativeDriver: false
-          }),
-        Animated.timing(thirdInterpolation,
-          {
-            toValue: 20,
-            delay: 1500,
-            duration: 500,
-            useNativeDriver: false
-          })
-      ]).start()
-  },
-  [])
-
+import { useState } from 'react'
+if (
+  Platform.OS === 'android' &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true)
+}
+export default function CardResultQuestionnaire({
+  grade,
+  bgColor,
+  msgCHU,
+  onOpenMsg,
+  onCallCHU,
+  nbrMsg,
+  notifReport,
+  nextReport,
+  lastCreatedReport,
+  text1,
+  text2,
+  text3,
+  text4,
+  text5,
+  text6,
+  text7,
+  text8,
+  text9,
+}) {
+  const [first1Value, setFirst1Value] = useState(false)
+  const [firstValue, setFirstValue] = useState(false)
+  const [secondValue, setSecondValue] = useState(false)
+  const [thirdValue, setThirdValue] = useState(false)
+  setTimeout(() => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
+    setFirst1Value(true)
+  }, 1000)
+  setTimeout(() => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
+    setFirstValue(true)
+  }, 2000)
+  setTimeout(() => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.spring)
+    setSecondValue(true)
+  }, 3000)
+  setTimeout(() => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.linear)
+    setThirdValue(true)
+  }, 4000)
   return (
     <View style={[styles.container, { backgroundColor: bgColor }]}>
       <View style={styles.headerContainer}>
@@ -59,108 +69,102 @@ export default function CardResultQuestionnaire (props) {
           {text1}
         </Text>
 
-        <Animated.Image style={[styles.iconContent, { opacity: firstOpacity }]} source={ Images.checkmarkCircle } />
+        <Image
+          style={[styles.iconContent, { opacity: first1Value ? 1 : 0 }]}
+          source={Images.checkmarkCircle}
+        />
       </View>
 
-      <Animated.Text style={[
-        styles.nextQstContent, Typography.body,
-        {
-          height: secondInterpolation.interpolate(
-            {
-              inputRange: [0, 100],
-              outputRange: [0, 100]
-            }
-          )
-        }
-      ]}>
+      {firstValue && (
+        <Text style={[styles.nextQstContent, Typography.body]}>
+          {text2}
 
-        {text2}
-
-        <Text style={[styles.colorDate, Typography.bodyBold]}>
-          {nextReport}
+          <Text style={[styles.colorDate, Typography.bodyBold]}>
+            {nextReport}
+          </Text>
         </Text>
-      </Animated.Text>
+      )}
 
-      <Animated.Text style={[
-        styles.nextQstContent, Typography.captionItalic,
-        {
-          height: thirdInterpolation.interpolate(
-            {
-              inputRange: [0, 100],
-              outputRange: [0, 100]
-            }
-          )
-        }
-      ]}>
+      {secondValue && (
+        <Text style={[styles.nextQstContent, Typography.captionItalic]}>
+          {text3}
 
-        {text3}
-
-        <Text style={styles.underText}>
-          {notifReport}
+          <Text style={styles.underText}>{notifReport}</Text>
         </Text>
-      </Animated.Text>
+      )}
 
-      <View style={[styles.bodyContainer]}>
-        {((nbrMsg !== 0 && grade === 2) || grade === 3 || grade === 4) &&
-          <Text style={[styles.resultTitle, Typography.captionBold]}>
-            {text4}{lastCreatedReport}
-          </Text>}
-
-        {(grade === 2) && nbrMsg !== 0 &&
-          <View style={styles.consultMsgContainer}>
-            <FontAwesome color="black" name="info-circle" size={20}/>
-
-            <Text style={[styles.consultMsgContent, Typography.bodyBold]}>
-              {text5}
+      {thirdValue && (
+        <View style={[styles.bodyContainer]}>
+          {((nbrMsg !== 0 && grade === 2) || grade === 3 || grade === 4) && (
+            <Text style={[styles.resultTitle, Typography.captionBold]}>
+              {text4}
+              {lastCreatedReport}
             </Text>
-          </View>}
+          )}
 
-        {(grade === 3 || grade === 4) &&
-          <View style={styles.chuContainer}>
-            <View style={styles.callChuContainer}>
-              <FontAwesome color="black" name="info-circle" size={20}/>
+          {grade === 2 && nbrMsg !== 0 && (
+            <View style={styles.consultMsgContainer}>
+              <FontAwesome color='black' name='info-circle' size={20} />
 
-              <Text style={[styles.callChuTitle, Typography.bodyBold]}>
-                {text9}
+              <Text style={[styles.consultMsgContent, Typography.bodyBold]}>
+                {text5}
               </Text>
             </View>
+          )}
 
-            <Text style={[styles.callChuContent, Typography.body]}>
-              {msgCHU}
-            </Text>
+          {(grade === 3 || grade === 4) && (
+            <View style={styles.chuContainer}>
+              <View style={styles.callChuContainer}>
+                <FontAwesome color='black' name='info-circle' size={20} />
 
-            {(grade === 2 || grade === 3 || grade === 4) &&
-            <Button
-              onPress={onCallCHU}
-              icon={ <FontAwesome name="phone" size={16} color="white" /> }
-              title={text7}
-              titleStyle={[Typography.button, styles.titleBtnCall]}
-              buttonStyle={styles.btnCallContent}
-            />
-            }
-          </View>}
+                <Text style={[styles.callChuTitle, Typography.bodyBold]}>
+                  {text9}
+                </Text>
+              </View>
 
-        {(grade === 2 || grade === 3 || grade === 4) && nbrMsg !== 0 &&
+              <Text style={[styles.callChuContent, Typography.body]}>
+                {msgCHU}
+              </Text>
+
+              {(grade === 2 || grade === 3 || grade === 4) && (
+                <Button
+                  onPress={onCallCHU}
+                  icon={<FontAwesome name='phone' size={16} color='white' />}
+                  title={text7}
+                  titleStyle={[Typography.button, styles.titleBtnCall]}
+                  buttonStyle={styles.btnCallContent}
+                />
+              )}
+            </View>
+          )}
+
+          {(grade === 2 || grade === 3 || grade === 4) && nbrMsg !== 0 && (
             <Text style={styles.symptomeContent}>
-              {nbrMsg}{text6}
-            </Text>}
+              {nbrMsg}
+              {text6}
+            </Text>
+          )}
 
-        {(grade === 2) && nbrMsg !== 0 &&
+          {grade === 2 && nbrMsg !== 0 && (
             <ButtonCureety
               onPress={onOpenMsg}
               title={text8}
               bgColor={theme.colors.link}
               buttonStyle={styles.conseilMsg}
-            />}
+            />
+          )}
 
-        {(grade === 4 || grade === 3) && nbrMsg !== 0 &&
+          {(grade === 4 || grade === 3) && nbrMsg !== 0 && (
             <ButtonCureety
               onPress={onOpenMsg}
               titleStyle={{ color: theme.colors.link }}
               title={text8}
               bgColor={'transparent'}
-              buttonStyle={styles.conseilMsg}/>}
-      </View>
+              buttonStyle={styles.conseilMsg}
+            />
+          )}
+        </View>
+      )}
     </View>
   )
 }
@@ -173,9 +177,9 @@ CardResultQuestionnaire.propTypes = {
   onCallCHU: PropTypes.string,
   notifReport: PropTypes.string,
   nextReport: PropTypes.string,
-  lastCreatedReport: PropTypes.string
+  lastCreatedReport: PropTypes.string,
 }
 
 CardResultQuestionnaire.defaultProps = {
-  bgColor: theme.colors.primary
+  bgColor: theme.colors.primary,
 }
--- components/dashboard/CardResultQuestionnaireStyle.js
+++ components/dashboard/CardResultQuestionnaireStyle.js
@@ -5,38 +5,38 @@ import theme from '../../constants/Theme'
 export const styles = StyleSheet.create({
   iconContent: {
     height: 25,
-    resizeMode: 'contain'
+    resizeMode: 'contain',
   },
   container: {
     minHeight: 145,
     width: width * 0.9,
     alignItems: 'center',
     borderRadius: 10,
-    justifyContent: 'space-between'
+    // justifyContent: 'space-between'
   },
   headerContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     marginTop: 30,
-    width: '90%'
+    width: '90%',
   },
   titleContent: {
     textTransform: 'uppercase',
     color: theme.colors.white,
-    flex: 1
+    flex: 1,
   },
   nextQstContent: {
     color: theme.colors.white,
     width: '90%',
     marginTop: 5,
-    marginBottom: 5
+    marginBottom: 5,
   },
   colorDate: {
-    color: theme.colors.white
+    color: theme.colors.white,
   },
   underText: {
-    textDecorationLine: 'underline'
+    textDecorationLine: 'underline',
   },
   bodyContainer: {
     borderRadius: 4,
@@ -44,25 +44,25 @@ export const styles = StyleSheet.create({
     backgroundColor: theme.colors.white,
     width: '95%',
     marginTop: 15,
-    marginBottom: 15
+    marginBottom: 15,
   },
   resultTitle: {
     marginTop: 20,
     marginBottom: 10,
     marginRight: 10,
     marginLeft: 10,
-    color: theme.colors.primary
+    color: theme.colors.primary,
   },
   consultMsgContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    width: '90%'
+    width: '90%',
   },
   consultMsgContent: {
     color: theme.colors.default,
     marginLeft: 5,
-    marginRight: 5
+    marginRight: 5,
   },
   chuContainer: {
     borderRadius: 4,
@@ -71,41 +71,41 @@ export const styles = StyleSheet.create({
     backgroundColor: theme.colors.gray,
     marginTop: 15,
     marginBottom: 15,
-    width: '95%'
+    width: '95%',
   },
   callChuContainer: {
     marginBottom: 5,
     marginTop: 14,
     flexDirection: 'row',
     alignItems: 'center',
-    width: '90%'
+    width: '90%',
   },
   callChuTitle: {
     marginLeft: 10,
-    color: theme.colors.default
+    color: theme.colors.default,
   },
   callChuContent: {
     marginBottom: 5,
     marginTop: 5,
     color: theme.colors.default,
-    width: '90%'
+    width: '90%',
   },
   titleBtnCall: {
     marginLeft: 10,
-    minWidth: '20%'
+    minWidth: '20%',
   },
   btnCallContent: {
     backgroundColor: theme.colors.link,
     marginBottom: 14,
     marginTop: 5,
-    borderRadius: 6
+    borderRadius: 6,
   },
   symptomeContent: {
     marginTop: 10,
-    color: theme.colors.label
+    color: theme.colors.label,
   },
   conseilMsg: {
     margin: 15,
-    borderRadius: 6
-  }
+    borderRadius: 6,
+  },
 })
